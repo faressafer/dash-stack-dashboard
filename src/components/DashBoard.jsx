02fbc2de4d1092c4cf867831f6c8519ad99a25fc
FaresOps <faressafer05@gmail.com>
2024-09-20 14:17:14 +0100

    test validated

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -8,16 +8,16 @@ const DashBoard = () => {
     <div className="ml-[240px] mt-[70px] p-6  border min-h-screen">
       <div className=" space-y-4">
         <h1 className="text-[#202224] font-nunito-sans text-[32px] font-bold tracking-[-0.114px]">
-          Broo ntesti
+          Dashboard
         </h1>
         <div className="w-full">
-          <Numbers />
+            <Numbers/>
         </div>
         <div className=" w-full">
-          <SalesDetails />
+            <SalesDetails/>
         </div>
         <div className=" w-full">
-          <DealsDetails />
+            <DealsDetails/>
         </div>
       </div>
     </div>
